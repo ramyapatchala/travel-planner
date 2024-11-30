84cb5ffb0d8225992c87161757989ef73579932b
ramyapatchala <rpatchal@syr.edu>
2024-11-30 01:34:44 -0500

    Update page1.py

--- page1.py
+++ page1.py
@@ -74,24 +74,8 @@ def get_Weather(location, API_key):
     url = urlbase + urlweather
     response = requests.get(url)
     data = response.json()
-
-    # Extract temperatures & Convert Kelvin to Celsius
-    temp = data['main']['temp'] - 273.15
-    feels_like = data['main']['feels_like'] - 273.15
-    temp_min = data['main']['temp_min'] - 273.15
-    temp_max = data['main']['temp_max'] - 273.15
-    humidity = data['main']['humidity']
     
-    return {
-        "location": location,
-        "temperature": round(temp, 2),
-        "feels_like": round(feels_like, 2),
-        "temp_min": round(temp_min, 2),
-        "temp_max": round(temp_max, 2),
-        "humidity": round(humidity, 2)
-    }
-
-
+    return data
 
 # Function to fetch places from Google Places API
 def fetch_places_from_google(query):
