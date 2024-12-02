661a09a1ff8becb225812908fdb20db35bf75ff8
ramyapatchala <rpatchal@syr.edu>
2024-12-02 18:10:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 
 import streamlit as st
 from streamlit_option_menu import option_menu
-
-import streamlit as st
-
 # Set page configuration (must be the first Streamlit command)
 st.set_page_config(page_title="Interactive Travel Guide Chatbot", page_icon="🌎", layout="wide")
-
-# Import your app logic or pages
-exec(open("page1.py").read())
+page1 = st.Page("page1.py", title="Explore")
+page2 = st.Page("page2.py", title="Itenary")
+pg = st.navigation([page1, page2])
+st.set_page_config(page_title="Trip Advisor")
+pg.run()
