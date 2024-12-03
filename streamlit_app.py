4665c0164b95bc41964becbf7e801911c9b20e6a
ramyapatchala <rpatchal@syr.edu>
2024-12-03 00:11:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ from streamlit_option_menu import option_menu
 st.set_page_config(page_title="Interactive Travel Guide Chatbot", page_icon="🌎", layout="wide")
 page1 = st.Page("page1.py", title="Explore")
 page2 = st.Page("page2.py", title="Itenary")
-page3 = st.Page("langchain.py", title="LangChain")
+page3 = st.Page("test_langchain.py", title="LangChain")
 pg = st.navigation([page1, page2, page3])
 pg.run()
